c58c5028262449d071bdd9883b5470735fbc483a
とーや <touyayoyo@gmiail.com>
2025-11-02 12:44:15 +0900

    Add initial frontend files including CSS, HTML, and SVG assets
    - Created a new CSS file for styling the frontend.
    - Added an HTML file as the main entry point for the application.
    - Included a Vite logo SVG file for branding in the application.

--- LifeLogAPI/obj/Debug/net8.0/LifeLogAPI.AssemblyInfo.cs
+++ LifeLogAPI/obj/Debug/net8.0/LifeLogAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LifeLogAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1c0d3a9653aaf48e7393e9b19c28bf6c22bfb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba274a4600fc22ecb105c129b1848f6c1ec8dae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LifeLogAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LifeLogAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
